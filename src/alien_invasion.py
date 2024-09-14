85e0004e3ee7703c5df8851629a174b84403b4dc
Dvleu <L21020400@veracruz.tecnm.mx>
2024-09-14 17:18:47 -0600

    Archivos agreados: src/Images/ship.bmp src/__pycache__/settings.cpython-312.pyc src/__pycache__/ship.cpython-312.pyc src/alien_invasion.py src/settings.py src/ship.py
    Se le agrego la nave espacial al juego ya al ejecutarse puede observarse la nave.

--- src/alien_invasion.py
+++ src/alien_invasion.py
@@ -2,18 +2,27 @@ import sys
 
 import pygame
 
+from settings import Settings
+from ship import Ship
+
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior. """
     def __init__(self):
         """Initialize the game, and create game resources.  """
         pygame.init()
-        self.clock = pygame.time.Clock()
-        self.screen = pygame.display.set_mode((1200,800))
-        pygame.display.set_caption("Alien invasion")
-        #?Set the background color.
-        self.bg_color = (230,230,230)
         
+        
+        
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
+        
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height)
+        )
+        pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
+
     def run_game(self):
             """Start the main loop for the game. """
             while True:
@@ -23,7 +32,8 @@ class AlienInvasion:
                         sys.exit()
                 
                 #?Redraw the screen during each pass through the loop 
-                self.screen.fill(self.bg_color)
+                self.screen.fill(self.settings.bg_color)
+                self.ship.blitme()
                         
                 #?Make the most recently drawn screen visible.
                 pygame.display.flip()
