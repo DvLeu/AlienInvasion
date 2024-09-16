97e69139effe152a2a9278b796aa8d24178b85c7
Dvleu <L21020400@veracruz.tecnm.mx>
2024-09-16 16:45:05 -0600

    New changes on the game now the bullets are on the game not implemented yet

--- src/settings.py
+++ src/settings.py
@@ -3,10 +3,16 @@ class Settings:
 
     def __init__(self):
         """Initialize the game's settings."""
-        # Screen settings
+        #? Screen settings
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
 
-        # Ship settings.
+        #? Ship settings.
         self.ship_speed = 1.5
+
+        #?Bullet settings.
+        self.bullet_speed = 2.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60,60,60)
