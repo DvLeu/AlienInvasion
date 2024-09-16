fbc6617dca45ea9b49d76d47264ef1c326018ca4
Dvleu <L21020400@veracruz.tecnm.mx>
2024-09-16 15:59:51 -0600

    Refactoring _check_events() and simplyfing some assets

--- src/alien_invasion.py
+++ src/alien_invasion.py
@@ -36,21 +36,25 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+            """Respond to keypresses"""
+            if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
+            elif event.key == pygame.K_LEFT:
                     self.ship.moving_left = True
 
-
-            elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
+    def _check_keyup_events(self,event):
+            """"""
+            if event.key == pygame.K_RIGHT:
                     self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:
+            elif event.key == pygame.K_LEFT:
                     self.ship.moving_left = False
-                
-                
+
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
         self.screen.fill(self.settings.bg_color)
