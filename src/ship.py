002b19bcbb74dc23e6bc248976eca2154b210ece
Dvleu <L21020400@veracruz.tecnm.mx>
2024-09-16 13:50:02 -0600

    The ship now can move faster using settings.py and the ship.py inhereting the speed

--- src/ship.py
+++ src/ship.py
@@ -6,7 +6,9 @@ class Ship :
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position. """
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
+        
         
         #? Load the ship image and get is rect 
         self.image = pygame.image.load('src\Images\ship.bmp')
@@ -15,16 +17,25 @@ class Ship :
         #? Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
+        #?Store a float for the ship's exact horizontal position
+        self.x = float(self.rect.x)
+
         #?Movement flag; start with a ship tha's not moving.
         self.moving_right = False
         self.moving_left = False
         
     def update(self):
         """Update the ship's position based on the movement flag. """
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -= 1
+        #?Update the ship's x value, not the rect
+
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        #?Update rect object from self.x
+        self.rect.x = self.x
 
 
     def blitme(self):
