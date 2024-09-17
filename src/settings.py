15b815e80eaafeca4497ee8fa5ad5c98930c0ed6
Dvleu <L21020400@veracruz.tecnm.mx>
2024-09-17 12:38:03 -0600

    Instructions added

--- src/settings.py
+++ src/settings.py
@@ -20,6 +20,6 @@ class Settings:
         self.bullets_allowed = 3
         self.alien_speed = 1.0
         self.fleet_drop_speed = 10 
-        #Fleet_direction of 1 represents right; -1 represents left.
+        #? Fleet_direction of 1 represents right; -1 represents left.
         self.Fleet_direction = 1
-
+        #! page 264 CHAPTER 13
