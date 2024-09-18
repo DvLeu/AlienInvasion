35dabb92ce0e098e93786c4b7b9c8ef93cdee1a5
Dvleu <L21020400@veracruz.tecnm.mx>
2024-09-18 00:56:14 -0600

    Now the game is partially playable.

--- src/alien_invasion.py
+++ src/alien_invasion.py
@@ -56,9 +56,12 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
+            
+            if self.game_active:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
+
             self._update_screen()
             self.clock.tick(60)
 
