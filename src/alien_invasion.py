e3ec8bc66ea6664a486ec3b8b1a41e3a31d86161
Dvleu <L21020400@veracruz.tecnm.mx>
2024-09-17 22:10:48 -0600

    Now when we shoot an alien the alien Dissapear and when we destroy all the fleet the fleet will regenerate

--- src/alien_invasion.py
+++ src/alien_invasion.py
@@ -85,6 +85,10 @@ class AlienInvasion:
         collisions = pygame.sprite.groupcollide(
             self.bullets, self.aliens, True, True
         )
+        if not self.aliens:
+            #? Destroy existing bullets  and create a new fleet
+            self.bullets.empty()
+            self._create_fleet()
 
     def _update_aliens(self):
         """Update the positions of all aliens in the fleet."""
